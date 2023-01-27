7c2f220089b1a79e888277128efd4af0d5c14405
DevxStudio <62635706+DevxStudio@users.noreply.github.com>
2023-01-27 19:46:47 +1100

    Update AssemblyInfo.cs

--- Builder/Properties/AssemblyInfo.cs
+++ Builder/Properties/AssemblyInfo.cs
@@ -14,6 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+
 // Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
 // для компонентов COM. Если необходимо обратиться к типу в этой сборке через
 // COM, следует установить атрибут ComVisible в TRUE для этого типа.
